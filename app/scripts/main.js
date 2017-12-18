beb7eeeaaabb4cdde40496afa2d5ddf36634f9b4
noah <noah@noahdeiMac.local>
2017-12-18 22:41:02 +0800

    终于把导航的动画学完了！

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,21 @@
 (function() {
   'use strict';
   // Your custom JavaScript goes here
-  
+
+  /**
+   * navbar
+   */
+   $('.ui.navbar .search-icon > i')
+    .click(function(){
+      let icon = $(this)
+      if (icon.text() === 'search'){
+        icon.text('close').parents('.content').addClass('searching')
+      }else{
+        icon.text('search').parents('.content').removeClass('searching')
+      }
+    })
+
+
+
 
 })();
